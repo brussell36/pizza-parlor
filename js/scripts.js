328f651a6ab81d6791d513d915ef4ac212f75b53
Ben Russell <benjaminrussell36@gmail.com>
2020-06-19 08:41:06 -0700

    Add html for dropdown to select pizza size and checkboxes for toppings.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Business Logic
+
+function Pizza(size, toppings) {
+  this.size = size,
+  this.toppings = toppings
+}
+
+// UI Logic
+
+$(document).ready(function() {
+  let pizza = new Pizza();
+
+});
