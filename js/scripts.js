bb0f3101dc41097ac4e409d86bd62344d8826762
Ben Russell <benjaminrussell36@gmail.com>
2020-06-19 11:56:43 -0700

    Change checkboxes to dropdowns and make new function to display choices and new method to add price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
 // Business Logic
 
-function Pizza(size, toppings) {
-  this.size = [size]
-  this.toppings = [toppings]
+function Pizza(size, topping1, topping2, topping3) {
+  this.size = size
+  this.topping1 = topping1,
+  this.topping2 = topping2,
+  this.topping3 = topping3
 }
 
-Pizza.prototype.addPizza = function(size, toppings) {
-  this.size.push(size);
-  this.toppings.push(toppings);
+Pizza.prototype.pizzaPrice = function() {
+  if (pizza.size === '12"') {
+    $("#pizza-final").append("You owe $15")
+  }
+  else if (pizza.size === '16"') {
+    $("#pizza-final").append("You owe $20")
+  }
+  else {
+    $("#pizza-final").append("You owe $25")
+  }
+}
+
+function showPizza(pizzaParam) {
+  $("#pizza-final").append(pizza.size + "<br>" + pizza.topping1 + "<br>" + pizza.topping2 + "<br>" + pizza.topping3 + "<br>")
 }
 
 // UI Logic
 
 $(document).ready(function() {
-  const newPizza = new Pizza();
+
   $("form#pizza-choose").submit(function(event) {
     event.preventDefault();
     $("#pizza-final").show();
     const pizzaSize = $("#pizza-size").val();
-    $("input:checkbox[name=toppings]:checked").each(function() {
-      const pizzaToppings = $(this).val();
-    });
-    newPizza.addPizza(pizzaSize, pizzaToppings);
-    console.log(newPizza);
+    const topping1 = $("#pizza-topping1").val();
+    const topping2 = $("#pizza-topping2").val();
+    const topping3 = $("#pizza-topping3").val();
+    const newPizza = new Pizza(pizzaSize, topping1, topping2, topping3);
+    showPizza(newPizza);
   });
 });
