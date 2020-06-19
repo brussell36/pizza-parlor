78ed891f059d341874ff652b205e61e17daff3d8
Ben Russell <benjaminrussell36@gmail.com>
2020-06-19 10:00:58 -0700

    Add function to show pizza details to user. Add method to push toppings and size into Pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 // Business Logic
 
 function Pizza(size, toppings) {
-  this.size = size,
-  this.toppings = toppings
+  this.size = size
+  this.toppings = [toppings]
+}
+
+Pizza.prototype.addToppings = function(toppings) {
+  this.toppings.push(toppings);
+}
+
+function showPizza(newPizza) {
+  $("#pizza-final").append(pizzaSize + "<br>")
+  $("#pizza-final").append(pizzaToppings + "<br>");
 }
 
 // UI Logic
 
 $(document).ready(function() {
+  const newPizza = new Pizza();
   $("form#pizza-choose").submit(function(event) {
     event.preventDefault();
     $("#pizza-final").show();
     const pizzaSize = $("#pizza-size").val();
-    $("#pizza-final").append(pizzaSize + "<br>")
     $("input:checkbox[name=toppings]:checked").each(function() {
       const pizzaToppings = $(this).val();
-      $("#pizza-final").append(pizzaToppings + "<br>");
-    })
+
+    });
   });
 });
