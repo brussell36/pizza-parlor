0f4af12446ebc6fdd096eb052db2ddad9e314fef
Ben Russell <benjaminrussell36@gmail.com>
2020-06-19 12:16:59 -0700

    Add showPizza function to output values to user when they press submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,34 @@ function Pizza(size, topping1, topping2, topping3) {
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  if (pizza.size === '12"') {
-    $("#pizza-final").append("You owe $15")
+  if (this.size === '12"') {
+    $("#pizza-final").append("You owe $15" + "<br>")
   }
-  else if (pizza.size === '16"') {
-    $("#pizza-final").append("You owe $20")
+  else if (this.size === '16"') {
+    $("#pizza-final").append("You owe $20" + "<br>")
   }
   else {
-    $("#pizza-final").append("You owe $25")
+    $("#pizza-final").append("You owe $25" + "<br>")
   }
 }
 
-function showPizza(pizzaParam) {
-  $("#pizza-final").append(pizza.size + "<br>" + pizza.topping1 + "<br>" + pizza.topping2 + "<br>" + pizza.topping3 + "<br>")
+function showPizza() {
+  $("#pizza-final").append(this.size + "<br>" + this.topping1 + "<br>" + this.topping2 + "<br>" + this.topping3 + "<br>")
 }
 
 // UI Logic
 
 $(document).ready(function() {
-
   $("form#pizza-choose").submit(function(event) {
     event.preventDefault();
     $("#pizza-final").show();
-    const pizzaSize = $("#pizza-size").val();
+    const size = $("#pizza-size").val();
     const topping1 = $("#pizza-topping1").val();
     const topping2 = $("#pizza-topping2").val();
     const topping3 = $("#pizza-topping3").val();
-    const newPizza = new Pizza(pizzaSize, topping1, topping2, topping3);
-    showPizza(newPizza);
+    const pizza = new Pizza(size, topping1, topping2, topping3);
+    console.log(pizza);
+    pizza.pizzaPrice();
+    showPizza();
   });
 });
