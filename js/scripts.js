545325aaa3dd98eb32193dc48dc9cdffcffec902
Ben Russell <benjaminrussell36@gmail.com>
2020-06-19 08:59:09 -0700

    Add UI logic to save user inputs of size and toppings and append them to page.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,14 @@ function Pizza(size, toppings) {
 // UI Logic
 
 $(document).ready(function() {
-  let pizza = new Pizza();
-
+  $("form#pizza-choose").submit(function(event) {
+    event.preventDefault();
+    $("#pizza-final").show();
+    const pizzaSize = $("#pizza-size").val();
+    $("#pizza-final").append(pizzaSize + "<br>")
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      const pizzaToppings = $(this).val();
+      $("#pizza-final").append(pizzaToppings + "<br>");
+    })
+  });
 });
